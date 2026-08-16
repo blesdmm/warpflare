53a2ecc8988e7da80fb3f3e1aba238f35173cd08
blesdmm <78246748+blesdmm@users.noreply.github.com>
2026-08-17 03:19:41 +0800

    Update src/utils/generate.ts

--- src/utils/generate.ts
+++ src/utils/generate.ts
@@ -97,7 +97,7 @@ export const generateShadowrocket = (
     const server = node.ip || node.server || "0.0.0.0";
     
     // 强制赋值端口 2408
-    const port = 2408; 
+   // const port = 2408; 
     
     const name = node.name || "Unknown";
 
